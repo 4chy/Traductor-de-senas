a4a57be1566988406495b10b07c5b00c855636be
4chy <alcides.car.gam@gmail.com>
2025-06-19 17:40:02 -0300

    Carpetas del proyecto 19/6

---
+++ Data.py
@@ -0,0 +1,43 @@
+#Importar liberias
+import cv2
+import os
+
+#Importar la clase
+import SeguimientoManos as sm
+
+#creacion de la carpeta
+nombre = "Letra_A"
+direccion = "F:/1. Proyecto final/lenguaje de señas/Vocales/data"
+carpeta = direccion + "/" + nombre
+
+#Si no esta creada la carpeta
+if not os.path.exists(carpeta):
+    print("CARPETA CREADA: ", carpeta)
+    #Creamos la carpeta
+    os.makedirs(carpeta)
+
+#Lectura de la camara
+cap = cv2.VideoCapture()
+
+#Cambiar la resolucion
+cap.set(3,1280)
+cap.set(4,720)
+
+#Declarar detector
+detector = sm.detectormanos(Confdeteccion=0.9)
+
+while True:
+
+    #Realizar la lectura de la cap
+    ret,frame = cap.read()
+
+    #Mostrar FPS
+    cv2.imshow("LENGUAJES VOCALES", frame)
+
+    #Leer nuestro teclado
+    t = cv2.waitKey(1)
+    if t == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
